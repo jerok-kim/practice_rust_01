4ea34bac0f0a849bb0c3e066f0a6c66899e6ca7d
jerok <jerok.kim@gmail.com>
2023-07-03 15:42:07 +0900

    practice rust

--- section_04/src/main.rs
+++ section_04/src/main.rs
@@ -1,3 +1,49 @@
+#[derive(Debug)]
+enum Color {
+    Red,
+    Black,
+    Navy,
+    Gray,
+    Green,
+}
+
+#[derive(Debug)]
+struct Car {
+    mpg: i32,
+    color: Color,
+    top_speed: i32,
+}
+
+impl Car {
+    fn new(mpg: i32, color: Color, top_speed: i32) -> Self {
+        Self {
+            mpg,
+            color,
+            top_speed,
+        }
+    }
+
+    fn set_mpg(&mut self, mpg: i32) {
+        self.mpg = mpg;
+    }
+
+    fn set_color(&mut self, color: Color) {
+        self.color = color;
+    }
+
+    fn set_top_speed(&mut self, top_speed: i32) {
+        self.top_speed = top_speed;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut car = Car::new(100, Color::Gray, 270);
+
+    println!("{:?}", car);
+    
+    car.set_mpg(80);
+    car.set_color(Color::Navy);
+    car.set_top_speed(300);
+
+    println!("{:?}", car);
 }
