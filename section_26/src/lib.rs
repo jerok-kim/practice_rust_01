d1640c53e68889a7f7e4eb13e5602db1b342be01
jerok <jerok.kim@gmail.com>
2023-07-10 22:13:24 +0900

    practice rust

--- section_26/src/lib.rs
+++ section_26/src/lib.rs
@@ -1,3 +1,126 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct NodeNotInGraph; // Custom error type if a node does not exist
+
+pub struct DirectedGraph {
+    adjacency_matrix: HashMap<String, Vec<(String, i32)>>,
+}
+
+pub struct UndirectedGraph {
+    adjacency_matrix: HashMap<String, Vec<(String, i32)>>,
+}
+
+pub trait Graph {
+    fn new() -> Self;
+    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
+
+    fn add_node(&mut self, node: &str) -> bool {
+        match self.adjacency_matrix().get(node) {
+            None => {
+                self.adjacency_matrix()
+                    .insert((*node).to_string(), Vec::new());
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
+        self.add_node(edge.0);
+        self.add_node(edge.1);
+
+        self.adjacency_matrix()
+            .entry(edge.0.to_string())
+            .and_modify(|e| {
+                e.push((edge.1.to_string(), edge.2));
+            });
+    }
+
+    fn neighbors(&mut self, node: &str) -> Result<&Vec<(String, i32)>, NodeNotInGraph> {
+        match self.adjacency_matrix().get(node) {
+            None => Err(NodeNotInGraph),
+            Some(i) => Ok(i),
+        }
+    }
+}
+
+impl Graph for DirectedGraph {
+    fn new() -> DirectedGraph {
+        DirectedGraph {
+            adjacency_matrix: HashMap::new(),
+        }
+    }
+
+    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
+        &mut self.adjacency_matrix
+    }
+}
+
+impl Graph for UndirectedGraph {
+    fn new() -> UndirectedGraph {
+        UndirectedGraph {
+            adjacency_matrix: HashMap::new(),
+        }
+    }
+
+    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
+        &mut self.adjacency_matrix
+    }
+
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
+        self.add_node(edge.0);
+        self.add_node(edge.1);
+
+        self.adjacency_matrix
+            .entry(edge.0.to_string())
+            .and_modify(|e| {
+                e.push((edge.1.to_string(), edge.2));
+            });
+
+        // Add in connecting edge.1 to edge.0 so that the graph is now undirected
+        self.adjacency_matrix()
+            .entry(edge.1.to_string())
+            .and_modify(|e| {
+                e.push((edge.0.to_string(), edge.2));
+            });
+    }
+}
+
+#[cfg(test)]
+mod test_undirected_graph {
+    use super::*;
+
+    #[test]
+    fn test_neighbors() {
+        let mut graph = UndirectedGraph::new();
+        graph.add_edge(("a", "b", 5));
+        graph.add_edge(("b", "c", 10));
+        graph.add_edge(("c", "a", 7));
+
+        assert_eq!(
+            graph.neighbors("a").unwrap(),
+            &vec![(String::from("b"), 5), (String::from("c"), 7)]
+        );
+    }
+}
+
+#[cfg(test)]
+mod test_directed_graph {
+    use super::*;
+
+    #[test]
+    fn test_neighbors() {
+        let mut graph = DirectedGraph::new();
+        graph.add_edge(("a", "b", 5));
+        graph.add_edge(("b", "c", 10));
+        graph.add_edge(("c", "a", 7));
+        graph.add_edge(("b", "a", 5));
+
+        assert_eq!(graph.neighbors("a").unwrap(), &vec![(String::from("b"), 5)]);
+        assert_eq!(
+            graph.neighbors("b").unwrap(),
+            &vec![(String::from("c"), 10), (String::from("a"), 5)]
+        );
+    }
 }
---
+++ section_27/src/lib.rs
@@ -0,0 +1,96 @@
+use std::collections::{HashSet, VecDeque};
+
+pub fn depth_first_search(graph: &Graph, root: Vertex, objective: Vertex) -> Option<Vec<u8>> {
+    let mut visited: HashSet<Vertex> = HashSet::new();
+    let mut history: Vec<u8> = Vec::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back(root);
+
+    while let Some(current_vertex) = queue.pop_front() {
+        history.push(current_vertex.value());
+
+        if current_vertex == objective {
+            return Some(history);
+        }
+
+        for neighbor in current_vertex.neighbors(graph).into_iter().rev() {
+            if visited.insert(neighbor) {
+                queue.push_front(neighbor)
+            }
+        }
+    }
+
+    None
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Vertex(u8);
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Edge(u8, u8);
+
+#[derive(Clone)]
+pub struct Graph {
+    vertices: Vec<Vertex>,
+    edges: Vec<Edge>,
+}
+
+impl Graph {
+    pub fn new(vertices: Vec<Vertex>, edges: Vec<Edge>) -> Self {
+        Graph { vertices, edges }
+    }
+}
+
+impl From<u8> for Vertex {
+    fn from(item: u8) -> Self {
+        Vertex(item)
+    }
+}
+
+impl Vertex {
+    pub fn value(&self) -> u8 {
+        self.0
+    }
+
+    pub fn neighbors(&self, graph: &Graph) -> VecDeque<Vertex> {
+        graph
+            .edges
+            .iter()
+            .filter(|e| e.0 == self.0)
+            .map(|e| e.1.into())
+            .collect()
+    }
+}
+
+impl From<(u8, u8)> for Edge {
+    fn from(item: (u8, u8)) -> Self {
+        Edge(item.0, item.1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dfs() {
+        let vertices = vec![1, 2, 3, 4, 5, 6, 7];
+        let edges = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7)];
+
+        let root = 1;
+        let objective = 7;
+
+        let correct = vec![1, 2, 4, 5, 3, 6, 7];
+
+        let graph = Graph::new(
+            vertices.into_iter().map(|v| v.into()).collect(),
+            edges.into_iter().map(|e| e.into()).collect(),
+        );
+
+        assert_eq!(
+            depth_first_search(&graph, root.into(), objective.into()),
+            Some(correct)
+        );
+    }
+}
