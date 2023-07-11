c487514097e101848a82c69add64b3d9baa04816
jerok <jerok.kim@gmail.com>
2023-07-11 16:30:31 +0900

    practice rust

--- section_27/src/lib.rs
+++ section_27/src/lib.rs
@@ -1,22 +1,48 @@
-use std::collections::{HashSet, VecDeque};
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 
-pub fn depth_first_search(graph: &Graph, root: Vertex, objective: Vertex) -> Option<Vec<u8>> {
-    let mut visited: HashSet<Vertex> = HashSet::new();
-    let mut history: Vec<u8> = Vec::new();
-    let mut queue = VecDeque::new();
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: usize,
+}
 
-    queue.push_back(root);
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-    while let Some(current_vertex) = queue.pop_front() {
-        history.push(current_vertex.value());
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost).then_with(|| self.position.cmp(&other.position))
+    }
+}
 
-        if current_vertex == objective {
-            return Some(history);
-        }
+struct Edge {
+    node: usize,
+    cost: usize,
+}
 
-        for neighbor in current_vertex.neighbors(graph).into_iter().rev() {
-            if visited.insert(neighbor) {
-                queue.push_front(neighbor)
+fn shortest_path(graph: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+    let mut dist: Vec<_> = (0..graph.len()).map(|_| usize::MAX).collect();
+
+    let mut visited = BinaryHeap::new();
+
+    dist[start] = 0;
+    visited.push(State { cost: 0, position: start });
+
+    while let Some(State { cost, position }) = visited.pop() {
+        if position == goal { return Some(cost); }
+
+        if cost > dist[position] { continue; }
+
+        for edge in &graph[position] {
+            let next = State { cost: cost + edge.cost, position: edge.node };
+
+            if next.cost < dist[next.position] {
+                visited.push(next);
+                dist[next.position] = next.cost;
             }
         }
     }
@@ -24,73 +50,19 @@ pub fn depth_first_search(graph: &Graph, root: Vertex, objective: Vertex) -> Opt
     None
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Vertex(u8);
-
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Edge(u8, u8);
-
-#[derive(Clone)]
-pub struct Graph {
-    vertices: Vec<Vertex>,
-    edges: Vec<Edge>,
-}
-
-impl Graph {
-    pub fn new(vertices: Vec<Vertex>, edges: Vec<Edge>) -> Self {
-        Graph { vertices, edges }
-    }
-}
-
-impl From<u8> for Vertex {
-    fn from(item: u8) -> Self {
-        Vertex(item)
-    }
-}
-
-impl Vertex {
-    pub fn value(&self) -> u8 {
-        self.0
-    }
-
-    pub fn neighbors(&self, graph: &Graph) -> VecDeque<Vertex> {
-        graph
-            .edges
-            .iter()
-            .filter(|e| e.0 == self.0)
-            .map(|e| e.1.into())
-            .collect()
-    }
-}
-
-impl From<(u8, u8)> for Edge {
-    fn from(item: (u8, u8)) -> Self {
-        Edge(item.0, item.1)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_dfs() {
-        let vertices = vec![1, 2, 3, 4, 5, 6, 7];
-        let edges = vec![(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7)];
+    fn test_dijkstra() {
+        let graph = vec![
+            vec![Edge { node: 1, cost: 6 }, Edge { node: 2, cost: 4 }, Edge { node: 3, cost: 1 }],
+            vec![Edge { node: 0, cost: 6 }, Edge { node: 2, cost: 3 }],
+            vec![Edge { node: 0, cost: 4 }, Edge { node: 1, cost: 3 }, Edge { node: 3, cost: 1 }],
+            vec![Edge { node: 0, cost: 1 }, Edge { node: 2, cost: 1 }],
+        ];
 
-        let root = 1;
-        let objective = 7;
-
-        let correct = vec![1, 2, 4, 5, 3, 6, 7];
-
-        let graph = Graph::new(
-            vertices.into_iter().map(|v| v.into()).collect(),
-            edges.into_iter().map(|e| e.into()).collect(),
-        );
-
-        assert_eq!(
-            depth_first_search(&graph, root.into(), objective.into()),
-            Some(correct)
-        );
+        assert_eq!(shortest_path(&graph, 0, 1), Some(5));
     }
 }
---
+++ section_28/src/mst.rs
@@ -0,0 +1,57 @@
+﻿use disjoint_sets::UnionFind;
+
+type Node = usize;
+type Weight = usize;
+
+struct Edge {
+    dest: Node,
+    weight: Weight,
+}
+
+type Graph = Vec<Vec<Edge>>;
+
+fn edges_by_weight(graph: &Graph) -> Vec<(Node, Node, Weight)> {
+    let mut edges = vec![];
+
+    for (src, dest) in graph.iter().enumerate() {
+        for edge in dest {
+            edges.push((src, edge.dest, edge.weight));
+        }
+    }
+
+    edges.sort_by_key(|&(_, _, weight)| weight);
+    edges
+}
+
+fn mst(graph: &Graph) -> Vec<(Node, Node)> {
+    let mut result = vec![];
+    let mut union_find = UnionFind::new(graph.len());
+
+    for (src, dest, _) in edges_by_weight(graph) {
+        if !union_find.equiv(src, dest) {
+            union_find.union(src, dest);
+            result.push((src, dest));
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_mst() {
+        let graph = vec![
+            vec![Edge { dest: 1, weight: 3 }, Edge { dest: 3, weight: 6 }, Edge { dest: 5, weight: 2 }],
+            vec![Edge { dest: 3, weight: 5 }, Edge { dest: 5, weight: 4 }, Edge { dest: 2, weight: 1 }],
+            vec![Edge { dest: 3, weight: 2 }, Edge { dest: 4, weight: 3 }],
+            vec![Edge { dest: 4, weight: 7 }],
+            vec![Edge { dest: 5, weight: 2 }],
+            vec![],
+        ];
+
+        assert_eq!(vec![(1, 2), (0, 5), (2, 3), (4, 5), (0, 1)], mst(&graph));
+    }
+}
